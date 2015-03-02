0acb432b0ee0aab8733ea14b60a34fcc3d676516
sivan1525 <siva20078@gmail.com>
2015-03-02 21:11:51 +0530

    redis change

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-url = "redis://127.0.0.1:6379/1"
+url = "redis://127.0.0.1:6379/1redis://redistogo:d3b29cd4b3aa5d8248ef3c784aaf6f6f@ray.redistogo.com:9823/"
 
 Sidekiq.configure_server do |config|
   config.redis = { url: url, namespace: 'sidekiq' }
