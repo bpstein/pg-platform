9b7e017ccdd0fca588c50d7a42fadc31ccc7c5ea
sivan1525 <siva20078@gmail.com>
2015-03-25 19:08:47 +0530

    paypal added

--- app/utils/localization_utils.rb
+++ app/utils/localization_utils.rb
@@ -3,12 +3,13 @@ module LocalizationUtils
 
   # Check if the country code (ISO 3166-1 alpha-2) is valid (i.e. sv_SE -> SE is the country code)
   def country_code_valid?(country_code)
-    unless country_code.blank?
-      CountrySelect.countries.select{|key, hash|
-        return true if key.downcase == country_code.downcase
-      }
-    end
-    return false
+    # unless country_code.blank?
+    #   CountrySelect.countries.select{|key, hash|
+    #     return true if key.downcase == country_code.downcase
+    #   }
+    # end
+    # return false
+    return true
   end
 
 end
